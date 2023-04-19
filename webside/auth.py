cb745f9d1d059e2bbce512b3ec3693462aef37c0
JeremyLi17 <HaotianLi0505@hotmail.com>
2023-04-19 14:53:00 -0400

    signup data verification and flush messages

--- webside/auth.py
+++ webside/auth.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+# By default it only accepte GET method
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +11,24 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if (request.method == 'POST'):
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Invalid Email! Email must be greater than 3 characters!', category='error')
+        elif len(firstName) < 2:
+            flash('Invalid First Name! First name must be greater than 1 character!', category='error')
+        elif password1 != password2:
+            flash('Passwords don\' match!', category='error')
+        elif len(password1) < 6:
+            flash('Password must be at least 6 characters!', category='error')
+        else:
+            # add user to database
+            flash('Account created!', category='success')
+        
     return render_template("sign_up.html")
